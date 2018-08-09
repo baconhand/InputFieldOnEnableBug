1fdc880b83e6b8341172ed2e4f56a36383c71cfc
Jeremy Carver <jeremy@squarewave-llc.com>
2018-08-09 00:52:20 -0600

    Add all files to reproduce InputField focus issue. Might be related to 637877

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour
+{
+	public GameObject inputField;
+	
+	private void OnEnable()
+	{
+		InputField field = inputField.GetComponent<InputField>();
+		field.ActivateInputField();
+	}
+
+}
---
+++ Assets/Scripts/KeyboardListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardListener : MonoBehaviour
+{
+	public GameObject console;
+	
+	void Update ()
+	{
+		if(Input.GetKeyUp(KeyCode.BackQuote))
+		{
+			ToggleConsole();
+		}		
+	}
+
+	private void ToggleConsole()
+	{
+		console.SetActive(!console.activeSelf);
+	}
+}
